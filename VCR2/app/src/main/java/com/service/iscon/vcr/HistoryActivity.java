081bc3fb8bb774caa16ffd51ce3d401d76892461
priyashirke <priyashirke21@gmail.com>
2017-01-26 05:26:02 +0530

    vcr

---
+++ VCR2/app/src/main/java/com/service/iscon/vcr/HistoryActivity.java
@@ -0,0 +1,20 @@
+package com.service.iscon.vcr;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    RecyclerView rv_history;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_item_user_history);
+
+        rv_history=(RecyclerView)findViewById(R.id.rv_history);
+
+
+    }
+}
